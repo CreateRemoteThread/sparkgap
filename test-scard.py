dd7f80beb87dfdf35b8b97b5edda7a7cab1d1b06
CreateRemoteThread <elegura.wolfe@gmail.com>
2020-05-12 04:37:37 -0400

    working save

--- test-scard.py
+++ test-scard.py
@@ -12,6 +12,7 @@ import uuid
 import random
 import numpy as np
 import random
+import serial
 
 class SIMController:
   def __init__(self,trigger=None):
@@ -77,20 +78,76 @@ class SIMController:
     r,sw1,sw2 = self.c.transmit(cmd)
     return (r,sw1,sw2)
 
+class SIMController2:
+  def __init__(self):
+    self.ser = serial.Serial("/dev/ttyUSB0",9600)
+
+  def _do_auth(self):
+    pass
+    
+  def _send_apdu(self,apdu):
+    self.ser.write(b'a')
+    self.ser.write(bytes([len(apdu)]))
+    self.ser.write(apdu)
+
+  def _select_file(self,file_id=None,aid=[]):
+    cmd = [0xa0, 0xa4, 0x00, 0x00, 0x02]
+    USIM_CLA = 0x00
+    cmd[0] = USIM_CLA
+    cmd[3] = 0x04
+    if len(aid) != 0:
+      cmd[2] = 0x04
+      cmd[4] = len(aid)
+      cmd += aid
+    else:
+      b1 = (file_id >> 8) & 0xFF
+      b2 = file_id & 0xFF
+      cmd += [b1,b2]
+    out = ""
+    for c in cmd:
+      out += "%02x " % c
+    print(out)
+    self._send_apdu(cmd)
+    # r,sw1,sw2 = self.c.transmit(cmd)
+    # return (r,sw1,sw2)
+
+  def _get_resp(self):
+    self.ser.write(b'd')
+    c = self.ser.read(1)
+    if c != b'#':
+      print("Expected #, got %02x" % ord(c))
+      return
+    respsize = ord(self.ser.read(1))
+    print("RESPSIZE %d" % respsize)
+    c = self.ser.read(respsize)
+    for cx in c:
+      print("R: %02x" % cx)
+
+  def _umts_auth(self,rand,autn):
+    cmd = [0x00, 0x88, 0x00, 0x81, 0x22]
+    cmd.append(len(rand))
+    cmd += rand
+    cmd.append(len(autn))
+    cmd += autn
+    self._send_apdu(cmd)
+    # r,sw1,sw2 = self.c.transmit(cmd)
+    # return (r,sw1,sw2)
+    
+import time
 if __name__ == "__main__":
-  t = triggerbuddy.TriggerBuddy()
-  print("Trigger OK")
-  sc = SIMController(t)
-  print ("SIM Up")
-  # sys.exit(0)
-  t.disarm()
-  t.processCommand("io 10")
-  t.processCommand("clk 1")
-  # t.processCommand("clk 78000") # nextg
-  t.processCommand("ns 1") # next
   print(" >> YOU MUST MANUALLY CAPTURE ON YOUR SCOPE <<") 
   print(" >> NO SCOPE AUTOMATION ON C = 1 <<") 
-  # t.arm()
-  sc.do_auth()
-  t.disarm()
+  sc = SIMController2()
+  time.sleep(0.5)
+  # sc._select_file(file_id=0x3F00)
+  # time.sleep(1.0)
+  sc._select_file(aid=[0xa0,0x00,0x00,0x00,0x87])
+  time.sleep(0.5)
+  next_rand = [random.randint(0,255) for _ in range(16)]
+  next_autn = [random.randint(0,255) for _ in range(16)] 
+  sc._umts_auth(next_rand,next_autn)
+  time.sleep(0.5)
+  sc._get_resp()
+  time.sleep(0.1)
+  sc.ser.close()
   sys.exit(0)
