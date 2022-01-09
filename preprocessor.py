b0f05f1ab9abce8cc731718c6ad8f9f671a256cd
lololol <lol@lol.com>
2022-01-08 23:14:41 -0500

    i like bananas

--- preprocessor.py
+++ preprocessor.py
@@ -217,14 +217,14 @@ def doCORR(tm_in):
   # support.filemanager.save(CONFIG_WRITEFILE,traces=traces[0:savedDataIndex],data=data[0:savedDataIndex],data_out=data_out[0:savedDataIndex])
 
 import copy
-def doSingleCWTDenoise(x_in,wavelet="db4",level=1):
-  x = copy.copy(x_in)
+def doSingleCWTDenoise(x,wavelet="db4",level=1):
+  # x = copy.copy(x_in) # lol wtf
   coeff = pywt.wavedec(x,wavelet,mode="per")
   sigma = mad( coeff[-level])
   uthresh = sigma * np.sqrt( 2 * np.log( len(x) ) )
   coeff[1:] = (pywt.threshold(i,value=uthresh,mode="soft") for i in coeff[1:])
   y = pywt.waverec(coeff,wavelet,mode="per")
-  del(x)
+  # del(x)
   return y
   
 from statsmodels.robust import mad
