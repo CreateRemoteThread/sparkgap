ae18aab2dbb271ea1c96b3524d5962a784983ed4
lolololol <lol@lol.com>
2022-09-14 13:21:47 +1000

    glitcher_pwr

--- nddla.py
+++ nddla.py
@@ -18,8 +18,8 @@ CFG_ATTACK = None
 
 BYTENUM_MIN = 0
 BYTENUM_MAX = 1
-KEYBYTE_MIN = 0x00
-KEYBYTE_MAX = 0xFF
+KEYBYTE_MIN = 0x70
+KEYBYTE_MAX = 0x80
 
 if __name__ == "__main__":
   opts, args = getopt.getopt(sys.argv[1:],"f:o:n:a:",["file=","offset=","numsamples=","attack="])
