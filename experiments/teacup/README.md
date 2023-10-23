e6109505675bd1ce94bbfbf6c43e239fa564c76e
lolololol <lol@lol.com>
2023-10-23 20:44:20 +1100

    updated emu_load

--- experiments/teacup/README.md
+++ experiments/teacup/README.md
@@ -1,3 +1,5 @@
 # teacup
 
 Camellia sinensis is a species of evergreen shrub or small tree in the flowering plant family Theaceae. Its leaves and leaf buds are used to produce the popular beverage tea. Common names include tea plant, tea shrub, and tea tree (unrelated to Melaleuca alternifolia, the source of tea tree oil, or the genus Leptospermum commonly called tea tree).
+
+arm-none-eabi-gcc -nostdlib -o hello.elf hello.c
--- experiments/teacup/emu_loader.py
+++ experiments/teacup/emu_loader.py
@@ -4,8 +4,8 @@ import sparkgap
 import sparkgap.filemanager
 import sys
 import rainbow
+from rainbow import HammingWeight,TraceConfig
 from rainbow.generics import rainbow_arm
-from rainbow.utils import hw
 import numpy as np
 import gc
 import random
@@ -17,19 +17,19 @@ if len(sys.argv) != 2:
 
 cs = None
 
-KEY_ADDR = 0x18904
-DATA_ADDR = 0x188fc
+KEY_ADDR = 0x92dc
+DATA_ADDR = 0x92d4
 
 for i in range(0,50):
   # print("Emulating run %d..." % i)
-  emu = rainbow_arm(sca_mode=True)
+  emu = rainbow_arm(trace_config=TraceConfig(register=HammingWeight()))
   emu.load(sys.argv[1])
   rand_input = np.array([random.randint(0,0xFF) for i in range(0,8)],dtype=np.uint8)
   emu[DATA_ADDR] = bytes(rand_input[0:4])
   emu[DATA_ADDR + 4] = bytes(rand_input[4:8])
-  emu.start(emu.functions["doXTEA"] , 0x84c8)
-  new_trace = np.fromiter(map(hw, emu.sca_values_trace),dtype=np.float32)
-  rand_output = emu[0x18d64:0x18d64+8]
+  emu.start(emu.functions["doXTEA"] , 0x82a8)
+  new_trace = np.fromiter(map(lambda event: event["register"], emu.trace),dtype=np.float32)
+  rand_output = emu[0x92ec:0x92ec+8]
   if cs is None:
     cs = sparkgap.filemanager.CaptureSet(tracecount=50,samplecount=len(new_trace),in_len=8,out_len=8)
     cs.addTrace(new_trace,rand_input,rand_output)
