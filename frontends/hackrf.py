427132254ee58da3f8e85020d474ef1bb12fb3f1
lolololol <lol@lol.com>
2025-06-19 22:49:23 +1000

    hackrf initial save

---
+++ frontends/hackrf.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# hackrf frontend.
+
+import sys
+import subprocess
+
+class CaptureInterface():
+  def __init__(self):
+    self.config = {}
+    self.config["samplecount"] = 5000
+    self.config["freq"] = 16e6
+    self.config["bw"] = 1e6
+    print("HackRF ext.trig. frontend ready")
+
+  def init(self):
+    print("hackrf frontend init()")
+    self.hackrf_proc = None
+
+  def capture(self):
+    print("hackrf acquire")
+    try:
+      stdout,stderr = self.hackrf_proc.communicate(timeout=5.0)
+    except subprocess.TimeoutExpired as e:
+      print("Error: subprocess timed out")
+      return [0]
+    self.hackrf_proc = None
+    print(stdout)
+    print(stderr)
+    return [0]
+
+  # hackrf_transfer -H -d <serial number> -a 0 -l 32 -g 32 -r rx1.cs8
+  def arm(self):
+    try:
+      self.hackrf_proc = subprocess.Popen(["hackrf_transfer","-H","-a","0","-l","32","-g","32","-n","10000","-r","-"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=False)
+    except:
+      print("hackrf: could not open process")
+      sys.exit(0)
+    print("hackrf: started process")
+  
+  def disarm(self):
+    print("Acquisition frontend disarmed")
+    print(self.hackrf_proc)
+
+  def close(self):
+    pass    
--- traceutil.py
+++ traceutil.py
@@ -6,30 +6,48 @@ import readline
 import sparkgap.filemanager
 
 def doGlue(cfg):
-  tm1 = sparkgap.filemanager.TraceManager(cfg["infile"])
+  tm1 = sparkgap.filemanager.TraceManager(cfg["infile1"])
   tm2 = sparkgap.filemanager.TraceManager(cfg["infile2"])
+  CFG_LABEL1 = None
+  CFG_LABEL2 = None
+  if "label1" in cfg.keys():
+    CFG_LABEL1 = cfg["label1"]
+    print("Loading tag 1 of %d" % CFG_LABEL1)
+  if "label2" in cfg.keys():
+    CFG_LABEL2 = cfg["label2"]
+    print("Loading tag 2 of %d" % CFG_LABEL2)
   if tm1.numPoints != tm2.numPoints:
     print("fatal: trace sets must have equal numbers of points")
     return
-  if len(tm1.data_in[0]) != len(tm2.data_in[i]):
+  if len(tm1.data_in[0]) != len(tm2.data_in[0]):
     print("fatal: trace set data_in must be equal in length")
     return
-  if len(tm1.data_out[0]) != len(tm2.data_out[i]):
-    print("fatal: trace set data_out must be equal in length")
+  if CFG_LABEL1 is None and CFG_LABEL2 is None:
+    if len(tm1.data_out[0]) != len(tm2.data_out[0]):
+      print("fatal: trace set data_out must be equal in length")
+      return
+  if (CFG_LABEL1 is None and CFG_LABEL2 is not None) or (CFG_LABEL1 is not None and CFG_LABEL2 is None):
+    print("fatal / fixme: either label both sets or label only one")
     return
   bigbonk = tm1.traceCount + tm2.traceCount
   cs_out = sparkgap.filemanager.CaptureSet(tracecount=bigbonk,samplecount=tm1.numPoints,in_len = len(tm1.data_in[0]),out_len = len(tm1.data_out[0]))
   for i in range(0,tm1.traceCount):
-    cs_out.addTrace(tm1.traces[i],tm1.data_in[i],tm1.data_out[i]) 
+    if CFG_LABEL1 is None:
+      cs_out.addTrace(tm1.traces[i],[int(CFG_LABEL1)] * 8,[int(CFG_LABEL1)] * 8) 
+    else:
+      cs_out.addTrace(tm1.traces[i],tm1.data_in[i],tm1.data_out[i])
   for i in range(0,tm2.traceCount):
-    cs_out.addTrace(tm2.traces[i],tm2.data_in[i],tm2.data_out[i]) 
+    if CFG_LABEL2 is None:
+      cs_out.addTrace(tm1.traces[i],[int(CFG_LABEL2)] * 8,[int(CFG_LABEL2)] * 8) 
+    else:    
+      cs_out.addTrace(tm2.traces[i],tm2.data_in[i],tm2.data_out[i]) 
   cs_out.save(cfg["writefile"])
   print("glue operation complete")
 
 import binascii
 
 def doPeek(cfg):
-  tm = sparkgap.filemanager.TraceManager(cfg["infile"])
+  tm = sparkgap.filemanager.TraceManager(cfg["infile1"])
   tc = len(tm.traces)
   for i in range(0,min(10,tc)):
     di = tm.getSingleData(i)
@@ -37,8 +55,8 @@ def doPeek(cfg):
     print("%s:%s" % (binascii.hexlify(di),binascii.hexlify(do)))
 
 dispatch = {}
-dispatch["glue"] = (doGlue,["infile","infile2","writefile"])
-dispatch["peek"] = (doPeek,["infile"])
+dispatch["glue"] = (doGlue,["infile1","infile2","writefile"])
+dispatch["peek"] = (doPeek,["infile1"])
 config = {}
 
 print(" >> traceutil.py")
